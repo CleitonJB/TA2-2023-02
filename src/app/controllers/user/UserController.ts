0089710fa08989886d608d0a9d650df7f7fd564c
Cleiton Braga <cleitonbraga56@gmail.com>
2023-08-08 01:44:23 -0300

    Fix semantics of '500' error message in 'getAll' method at 'UserController'

--- src/app/controllers/user/UserController.ts
+++ src/app/controllers/user/UserController.ts
@@ -22,7 +22,7 @@ class UserController implements IUserModel<UserVM> {
                 return response.status(200).json({ status: 200, data: actionReturn });
             }
         } catch (error) {
-            return response.status(500).json({ status: 500, error: `Erro ao obter usuário: ${error}` });
+            return response.status(500).json({ status: 500, error: `Erro ao obter a lista de Usuários: ${error}` });
         }
     }
 
