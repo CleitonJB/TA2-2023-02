700c739ec90e8e2459706bd37f191c812a64c04e
Fernando Carvalho <fernando.carvalho.iff@gmail.com>
2023-08-08 19:57:17 -0300

    Add supertest 'app' file

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+import { RouterFactory } from "./app/router/RoutersFactory";
+
+const SERVER_PORT: number = 3333;
+
+const app = express();
+
+app.use(express.json());
+
+RouterFactory()
+    .then((routers: express.Router) => app.use(routers))
+    .catch((error: string) => console.error(`Erro ao fabricar rotas: ${error}`));
+
+app.get('/', (request, response) => {
+    response.json({ staus: 'OK', message: "Estamos trabalhando nas coisas por aqui... :)" });
+});
+
+export { app }
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,9 @@
 import express from "express";
 
 import { RouterFactory } from "./app/router/RoutersFactory";
+import { app } from "./app"
 
 const SERVER_PORT: number = 3333;
-
-const app = express();
-
-app.use(express.json());
-
-RouterFactory()
-    .then((routers: express.Router) => app.use(routers))
-    .catch((error: string) => console.error(`Erro ao fabricar rotas: ${error}`));
-
-app.get('/', (request, response) => {
-    response.json({ staus: 'OK', message: "Estamos trabalhando nas coisas por aqui... :)" });
-});
 
 app.listen(SERVER_PORT, () => {
     console.log(`Server started on: http://localhost:${SERVER_PORT}`);
