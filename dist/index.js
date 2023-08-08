3be433f34c4b2333fe6f80b482d12fec1bb1331a
Cleiton Braga <cleitonbraga56@gmail.com>
2023-08-08 00:39:29 -0300

    Build project

--- dist/index.js
+++ dist/index.js
@@ -4,38 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const RoutersFactory_1 = require("./app/router/RoutersFactory");
 const SERVER_PORT = 3333;
 const app = (0, express_1.default)();
-//!const router = new Routers(null, null, null, null);
 app.use(express_1.default.json());
-//!app.use(router);
+(0, RoutersFactory_1.RouterFactory)()
+    .then((routers) => app.use(routers))
+    .catch((error) => console.error(`Erro ao fabricar rotas: ${error}`));
 app.get('/', (request, response) => {
-    // const user: UserVM | null = createUser();
-    // console.log(user);
-    // if(user == null) {
-    //     response.json({ message: 'ERROR'});
-    // } else {
-    //     response.json({ message: 'OK', user });
-    // }
     response.json({ staus: 'OK', message: "Estamos trabalhando nas coisas por aqui... :)" });
 });
-function createUser() {
-    // try {
-    //     const role = new RoleController({ id: '132783n928d29d2182313v21', descricao: 'ADM'});
-    //     const newUser: UserVM = {
-    //         id:    "3129u1g3192312n0n103",
-    //         nome:  "Cleiton",
-    //         email: "cleitonbraga56@gmail.com",
-    //         senha: "9bu123",
-    //         role:  role.get(),
-    //     };
-    //     const user = new UserController(newUser);
-    //     return user.get();
-    // } catch (error) {
-    //     new Error(`Erro ao criar usuário: ${JSON.stringify(error)}`);
-    //     return null;
-    // }
-}
 app.listen(SERVER_PORT, () => {
     console.log(`Server started on: http://localhost:${SERVER_PORT}`);
 });
