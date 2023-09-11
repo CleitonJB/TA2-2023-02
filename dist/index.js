661f2c616079e5ba24900c65d5d813615cc30702
Cleiton Braga <cleitonbraga56@gmail.com>
2023-09-11 02:09:44 -0300

    Change app server port form '3333' to '8081' cause AWS Error

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,8 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const RoutersFactory_1 = require("./app/router/RoutersFactory");
-const SERVER_PORT = 3333;
-const app = (0, express_1.default)();
-app.use(express_1.default.json());
-(0, RoutersFactory_1.RouterFactory)()
-    .then((routers) => app.use(routers))
-    .catch((error) => console.error(`Erro ao fabricar rotas: ${error}`));
-app.get('/', (request, response) => {
-    response.json({ staus: 'OK', message: "Estamos trabalhando nas coisas por aqui... :)" });
-});
-app.listen(SERVER_PORT, () => {
+const app_1 = require("./app");
+const SERVER_PORT = process.env.PORT || 8081;
+app_1.app.listen(SERVER_PORT, () => {
     console.log(`Server started on: http://localhost:${SERVER_PORT}`);
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 import { RouterFactory } from "./app/router/RoutersFactory";
 import { app } from "./app"
 
-const SERVER_PORT: number = 3333;
+const SERVER_PORT: string | number = process.env.PORT || 8081;
 
 app.listen(SERVER_PORT, () => {
     console.log(`Server started on: http://localhost:${SERVER_PORT}`);
